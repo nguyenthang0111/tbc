dc4ab87411f106f4257c992bf6297ae01c9421fc
Nguyen Thang <thang.nd194169@sis.hust.edu.vn>
2023-06-28 13:44:46 +0700

    feat: init project

---
+++ web/frontend/pages/Step1Screen.jsx
@@ -0,0 +1,19 @@
+import {
+    Card,
+    Page,
+    Layout,
+    TextContainer,
+    Image,
+    Stack,
+    Link,
+    Text,
+  } from "@shopify/polaris";
+  import { TitleBar } from "@shopify/app-bridge-react";
+  import { useTranslation, Trans } from "react-i18next";
+  
+  export default function Step1Screen() {
+    return (
+      <Text>Step 1 Screen</Text>
+    );
+  }
+  
---
+++ web/frontend/pages/Step2Screen.jsx
@@ -0,0 +1,19 @@
+import {
+    Card,
+    Page,
+    Layout,
+    TextContainer,
+    Image,
+    Stack,
+    Link,
+    Text,
+  } from "@shopify/polaris";
+  import { TitleBar } from "@shopify/app-bridge-react";
+  import { useTranslation, Trans } from "react-i18next";
+  
+  export default function Step2Screen() {
+    return (
+      <Text>Step 2 Screen</Text>
+    );
+  }
+  
