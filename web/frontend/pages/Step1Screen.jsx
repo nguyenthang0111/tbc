d4690d69128d3c2001a26b629c253ebf3103137e
your name <phuong.caothithu1@gmail.com>
2023-07-03 02:06:10 +0700

    theme2

--- web/frontend/pages/Step1Screen.jsx
+++ web/frontend/pages/Step1Screen.jsx
@@ -13,6 +13,7 @@ import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation, Trans } from "react-i18next";
 import {Divider, VerticalStack} from '@shopify/polaris';
 import {Grid, LegacyCard} from '@shopify/polaris';
+
 //import { Theme1 } from "../components";
 import { ProductsCard } from "../components";
 export default function Step1Screen() {
@@ -21,21 +22,49 @@ export default function Step1Screen() {
     <Page fullWidth>
     <Grid>
       <Grid.Cell columnSpan={{xs: 3, sm: 3, md: 2, lg: 6, xl: 6}}>
-        <LegacyCard title="Sales" sectioned>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
+        <LegacyCard title="Table of contents" sectioned>
+
+          <Text as="h1" variant="headingXs">
+            [Heading1]
+          </Text>
+          <Divider borderColor="border-inverse" />
+    
+          <List type="bullet">
+          <List.Item>Heading2</List.Item>
+          <List.Item>Heading3</List.Item>
+          <List.Item>Heading4</List.Item>
+          </List>
+          <Text as="h1" variant="headingXs">
+            [Heading1]
+          </Text>
+          <Divider borderColor="border-inverse" />
+    
+          <List type="bullet">
+          <List.Item>Heading2</List.Item>
+          <List.Item>Heading3</List.Item>
+          <List.Item>Heading4</List.Item>
+          </List>
         </LegacyCard>
       </Grid.Cell>
       <Grid.Cell columnSpan={{xs: 3, sm: 3, md: 2, lg: 6, xl: 6}}>
-        <LegacyCard title="Sales" sectioned>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
-          <p>View a summary of your online store’s sales.</p>
+        <LegacyCard title="table of Contents" sectioned>
+
+          <p>Heading1</p>
+          <Divider borderColor="border-inverse" />
+          <p>Heading2</p>
+          <Divider borderColor="border-inverse" />
+          <p>Heading3</p>
+          <Divider borderColor="border-inverse" />
+          <p>Heading4</p>
+          <Divider borderColor="border-inverse" />
+          <p>Heading1</p>
+          <Divider borderColor="border-inverse" />
+          <p>Heading2</p>
+          <Divider borderColor="border-inverse" />
+          <p>Heading3</p>
+          <Divider borderColor="border-inverse" />
+          <p>Heading4</p>
+          <Divider borderColor="border-inverse" />
         </LegacyCard>
       </Grid.Cell>
       <Grid.Cell columnSpan={{xs: 3, sm: 3, md: 2, lg: 6, xl: 6}}>
