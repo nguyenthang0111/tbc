8cce0b5849e1c63261e6506247e7a633b313262c
unknown <leanhhado11012000@gmail.com>
2023-07-14 15:19:43 +0700

    sua lai item vs itemlist add all dc

--- web/frontend/components/ListItem.jsx
+++ web/frontend/components/ListItem.jsx
@@ -3,28 +3,57 @@ import { Toast } from "@shopify/app-bridge-react";
 import { useTranslation } from "react-i18next";
 import { useAppQuery, useAuthenticatedFetch } from "../hooks";
 import { Item } from "./Item";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 export function ListItem({ data }) {
-  const addAll = () => {
-    alert("click button add all")
-  }
+  const [isAllButtonActive, setIsAllButtonActive] = useState(false);
+  const [isOnButtonActiveMap, setIsOnButtonActiveMap] = useState({});
+
+  const handleAddAllClick = () => {
+    setIsAllButtonActive(true);
+    const newIsOnButtonActiveMap = {};
+    data.forEach((article) => {
+      newIsOnButtonActiveMap[article.id] = true;
+    });
+    setIsOnButtonActiveMap(newIsOnButtonActiveMap);
+  };
+
+  const handleButtonClick = (articleId) => {
+    const newIsOnButtonActiveMap = {
+      ...isOnButtonActiveMap,
+      [articleId]: !isOnButtonActiveMap[articleId],
+    };
+    setIsOnButtonActiveMap(newIsOnButtonActiveMap);
+  };
+
+  useEffect(() => {
+    if (isAllButtonActive) {
+      const newIsOnButtonActiveMap = {};
+      data.forEach((article) => {
+        newIsOnButtonActiveMap[article.id] = true;
+      });
+      setIsOnButtonActiveMap(newIsOnButtonActiveMap);
+    }
+  }, [isAllButtonActive, data]);
+
   return (
     <div>
-      <Scrollable shadow style={{height: '200px'}} focusable>
+      <Scrollable shadow style={{ height: '200px' }} focusable>
         <LegacyCard>
           {data.map((article, index) => (
-            <LegacyCard.Section>
-              <Item key={index} item={ article }/>
+            <LegacyCard.Section key={index}>
+              <Item
+                item={article}
+                isOnButtonActive={isOnButtonActiveMap[article.id]}
+                onClick={() => handleButtonClick(article.id)}
+              />
             </LegacyCard.Section>
           ))}
         </LegacyCard>
       </Scrollable>
-      <div style={{textAlign:'right', paddingTop:'10px'}}>
-        <Button onClick={() => addAll()}>Add all article</Button>
+      <div style={{ textAlign: 'right', paddingTop: '10px' }}>
+        <Button onClick={handleAddAllClick}>Add all article</Button>
       </div>
     </div>
   );
 }
-
